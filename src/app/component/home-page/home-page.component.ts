ca16d05718795b231b3732e6b36449b653331ca5
Giovane Roberti Tafine <giovane.tafine@hcswifi.com.br>
2020-07-10 10:54:38 -0300

    Adding prominence to the selected

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -19,4 +19,22 @@ export class HomePageComponent implements OnInit {
             () => console.log(this.posts)
         );
     }
+
+    // Verificando a nova lista dos selecioanados para destacar
+    onSelectedFilter(event) {
+        console.log(event);
+        console.log(this.dataService.searchOption);
+
+        this.posts.map((post) => {
+            var res = this.dataService.searchOption.filter((search) => search.id === post.id);
+            var index = 0;
+            if (res.length > 0) {
+                index = this.posts.findIndex((post) => post.id === res[0].id);
+                this.posts[index] = { ...this.posts[index], isActive: true };
+            } else {
+                index = this.posts.findIndex((posts) => posts.id === post.id);
+                this.posts[index] = { ...this.posts[index], isActive: false };
+            }
+        });
+    }
 }
