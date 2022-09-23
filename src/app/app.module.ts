2cf42c62805616c42efbeedfe2752cf8f754d3cd
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-09-23 11:28:50 -0300

    Atualizando angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import { SearchBarComponent } from './component/search-bar/search-bar.component'
 
 import { HttpClientModule } from '@angular/common/http';
 
-import {
-    MatIconModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    MatChipsModule,
-    MatFormFieldModule,
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
